058f11872c7c3ed52cd8cf2fe5f81fb33601d998
Ashish1965 <ashishrajput1272011@gmail.com>
2024-02-27 21:16:21 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,54 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import React from "react";
+import { IoCallOutline } from "react-icons/io5";
+import { FaFacebook } from "react-icons/fa";
+import { SiTorbrowser } from "react-icons/si";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section>
+      <div className="flex justify-start md:justify-center">
+        <div className="md:w-1/3 w-1/2">
+        <img src="asserts/logo.png" alt="" />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex md:flex-row gap-x-5 flex-col mx-5 justify-center items-center">
+      <div className="md:w-1/3">
+          <img src="asserts/1.png" alt="" />
+        </div>
+        <div className="md:w-2/3">
+        <h1 className="py-2 font-bold text-lg">C.R.L. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h1>
+        <ul className="list-disc ml-10">
+          <li className="text-sm font-semibold">C.R.1.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+          <li className="text-sm font-semibold">C.R.1. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
+        </ul>
+          <img src="asserts/2.png" alt="" />
+          <p className="text-sm font-semibold">Government of India has awarded the <span className="font-bold">"National Energy Conservation Award 2018"</span>. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex justify-center items-center p-5">
+        <p className="text-sm font-semibold">INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
       </div>
-    </main>
+      <div className="px-20">
+        <img src="asserts/3.png" alt="" />
+        <p className="text-center text-sm p-5">Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</p>
+        <hr className="border-y-2px mx-20 border-red-600" />
+      </div>
+      <div className="flex items-center justify-center flex-col p-5">
+        <h1 className="py-2 font-bold text-lg">C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h1>
+        <p className="text-sm font-semibold text-center mt-5">CHEMICALS & PROCESS <span className="text-red-500">|</span> POWER <span className="text-red-500">|</span> WATER & WASTE WATER <span className="text-red-500">|</span> OILS & GAS <span className="text-red-500">|</span> PHARMA <span className="text-red-500">|</span> SUGARS & DISTILLERIES <span className="text-red-500">|</span> PAPER & PULP <span className="text-red-500">|</span> MARINE & DEFENCE <span className="text-red-500">|</span> METAL & MINING</p>
+        <p className="text-sm font-semibold text-center mt-5">FOOD & BEVERAGE <span className="text-red-500">|</span> PETROCHEMICAL & REFINERIES <span className="text-red-500">|</span> SOLAR <span className="text-red-500">|</span> BUILDING <span className="text-red-500">|</span> HVAC <span className="text-red-500">|</span> FIRE FIGHTING <span className="text-red-500">|</span> AGRICULTURE & RESIDENTIAL</p>
+      </div>
+      <div className="flex flex-col md:flex-row w-full h-28 bg-red-500 justify-center">
+        <div className="md:w-1/3 flex flex-row justify-center md:items-center md:h-full">
+        <a href="/" className="flex flex-row gap-3"><IoCallOutline className="bg-white rounded-full w-6 h-6"/><p className="text-white">Toll free 1800 200 1234</p></a>
+        </div>
+        <div className="md:w-1/3 flex flex-row justify-center md:items-center md:h-full">
+        <a href="/" className="flex flex-row gap-3"><FaFacebook className="bg-white rounded-full w-6 h-6"/><p className="text-white">www.facebook.com/cripumps</p></a>
+        </div>
+        <div className="md:w-1/3 flex flex-row justify-center md:items-center md:h-full">
+        <a href="" className="flex flex-row gap-3"><SiTorbrowser className="bg-white rounded-full w-6 h-6"/><p className="text-white">www.crigroups.com</p></a>
+        </div>
+      </div>
+    </section>
   );
 }
